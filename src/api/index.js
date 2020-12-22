819095375344aed0385c67ebf991a969e6c5b206
Barbara Masinde <barbzlornah@gmail.com>
2020-12-22 15:26:54 +0300

    new commit

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,9 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-        const {data: {confirmed,recovered,deaths,lastupdate}} = await axios.get(url);
+        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
 
-        const modifiedData = {confirmed,recovered,deaths,lastupdate }
+        const modifiedData = {confirmed,recovered,deaths,lastUpdate }
 
         return modifiedData;
     } catch (error) {
