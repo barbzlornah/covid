fe0f34b692242cf4128920075fcbd26b63a6f25a
Barbara Masinde <barbzlornah@gmail.com>
2021-01-02 10:47:21 +0300

    new commit

--- src/api/index.js
+++ src/api/index.js
@@ -6,28 +6,39 @@ export const fetchData = async () => {
     try {
         const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
 
-        const modifiedData = {confirmed,recovered,deaths,lastUpdate }
+       return {confirmed,recovered,deaths,lastUpdate };
 
-        return modifiedData;
     } catch (error) {
         
     }
 
 }
+// export const fetchDailyData = async () => {
+//     try {
+//         const { data } = await axios.get('${url}/daily');
+
+//         const modifiedData = data.map((dailyData) =>({
+//             confirmed: dailyData.confirmed.total,
+//             deaths: dailyData.deaths.total,
+//             date: dailyData.reportDate,
+
+//         }));
+        
+//         return modifiedData;
+
+//     }catch(error){
+
+//     }
+    
+// }
+
+// Instead of Global, it fetches the daily data for the US
 export const fetchDailyData = async () => {
     try {
-        const { data } = await axios.get('${url}/daily');
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
 
-        const modifiedData = data.map((dailyData) =>({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-
-        }));
-        return modifiedData;
-
-    }catch(error){
-
+      return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    } catch (error) {
+      return error;
     }
-    
-}
+  };
--- src/componets/Charts/Charts.jsx
+++ src/componets/Charts/Charts.jsx
@@ -11,7 +11,7 @@ const Charts = () => {
             setDailyData(await fetchDailyData());
         }
         
-
+        
         fetchAPI();
 
     }); 
